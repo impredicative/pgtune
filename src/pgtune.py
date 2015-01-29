4346f1907ff9308e2401a59ecc379154058f9af8
Impredicative <impredicative@users.noreply.github.com>
2015-01-29 15:00:48 -0600

    argparse updates

--- src/pgtune.py
+++ src/pgtune.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 '''
-pgtune -- postgresql.conf performance tuner
+pgtune -- postgresql.conf tuner
 
 pgtune prints generalized performance optimizations for postgresql.conf
 given the optional inputs max_connections and allocated memory fraction. The
@@ -10,8 +10,8 @@ values can be saved to a file which can be used by postgresql.conf with the
 include directive, as for example:
   include 'postgresql.conf.custom'
 
-For usage help:
-  $ pgtune -h
+For help:
+  $ ./pgtune.py -h
 
 https://github.com/impredicative/pgtune
 '''
@@ -22,12 +22,14 @@ import collections
 import math
 import os
 
-MEM_TOTAL = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # bytes
+settings = {
+'mem_total': os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # bytes
+}
 
 
 def format_bytes(n):
 
-    units = ('', 'KB', 'MB', 'GB')  # Restricted per section 18.1.1 in v9.2.
+    units = ('', 'kB', 'MB', 'GB')  # Restricted per section 18.1.1 in v9.2.
     base = 1024
     decrement_threshold = 0.2  # experimental
     divisor_max = len(units) - 1
@@ -50,13 +52,18 @@ def parse_args():
     parser = argparse.ArgumentParser(description='postgresql.conf tuner')
 
     parser.add_argument('-c', '--max_connections', dest='max_connections',
-                        type=int, default=100,
-                        help="minimally necessary maximum connections (default: %(default)s)")
+                        type=lambda s: max(1, int(s)),
+                        default=100,
+                        help='minimally necessary maximum connections '
+                             '(default: %(default)s)')
 
-    mem_str = format_bytes(MEM_TOTAL)
+    mem_str = format_bytes(settings['mem_total'])
     parser.add_argument('-f', '--mem_fraction', dest='mem_fraction',
-                        type=float, default=1.0,
-                        help="fraction of total physical memory ({}) to use (default: %(default)s)".format(mem_str))
+                        type=lambda s: min(1, max(0, float(s))),
+                        default=1.0,
+                        help=('fraction (>0 to 1.0) of total physical memory '
+                              '({}) to consider '
+                              '(default: %(default)s)').format(mem_str))
 
 #     parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
 #     parser.add_argument("-r", "--recursive", dest="recurse", action="store_true", help="recurse into subfolders [default: %(default)s]")
@@ -66,20 +73,28 @@ def parse_args():
 #     parser.add_argument('-V', '--version', action='version', version=program_version_message)
 #     parser.add_argument(dest="paths", help="paths to folder(s) with source file(s) [default: %(default)s]", metavar="path", nargs='+')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+    for k in ('max_connections', 'mem_fraction'):
+        settings[k] = getattr(args, k)
 
 
-def tune_conf(args):
+def tune_conf():
 
-    mem_fractional = int(MEM_TOTAL * args.mem_fraction)  # implicit floor
+    settings['mem_fractional'] = int(settings['mem_total'] *
+                                     settings['mem_fraction']) # implicit floor
+    settings['mem_fractional'] = max(1, settings['mem_fractional'])
     conf = c = collections.OrderedDict()
 
-    c['max_connections'] = args.max_connections
+    c['max_connections'] = settings['max_connections']
 
     return conf
 
 
 def print_conf(conf):
+
+    print('# pgtune configuration for connections={} and memory={}.\n'
+          .format(settings['max_connections'],
+                  format_bytes(settings['mem_fractional'])))
 
     for i in conf.items():
         print('{} = {}'.format(*i))
@@ -87,8 +102,8 @@ def print_conf(conf):
 
 def main():
 
-    args = parse_args()
-    conf = tune_conf(args)
+    parse_args()
+    conf = tune_conf()
     print_conf(conf)
 
 
